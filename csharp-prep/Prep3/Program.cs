493f9dd2ef9831ed7ad6ce084e354b24ae30ef5f
jarom-bradshaw <bra23041@byui.edu>
2024-09-25 22:33:20 -0600

    Did Program.CS for prep 3. Number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+        string userGuess = "";
+        int userNumber = 0;
+
+        while (userNumber != number)
+        // How can I use Try to handle for user error if the input is not an interger? Can I use TryParse?
+        {
+            Console.WriteLine("What is the magic number? ");
+            
+            userGuess = Console.ReadLine();
+            userNumber = int.Parse(userGuess);
+
+            if (userNumber < number)
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            else if (userNumber > number)
+            { 
+                Console.WriteLine("Too high! Try again.");
+            }
+            else
+            {
+                Console.Write("Congratulations! You guessed the magic number. ");
+            }
+        }
     }
 }
