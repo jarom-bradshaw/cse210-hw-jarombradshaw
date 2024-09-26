873ed2f5836ab14048648bf4468b8116155c0de4
jarom-bradshaw <bra23041@byui.edu>
2024-09-25 21:50:13 -0600

    Changed code to finish Prep1 for james bond, added calculator where the old sandbox assignment was. Oh well I guess. No more sandbox, unless you make a new one.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Diagnostics;
 
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string second_name = Console.ReadLine();
+        Console.Write($"Your name is {second_name}, {first_name} {second_name}.");
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
     }
 }
